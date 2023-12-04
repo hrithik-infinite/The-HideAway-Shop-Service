95b444fbfcb6c2b724b62d5df6405007b76031c1
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-04 21:37:57 +0530

    initial commit

---
+++ src/main/java/com/hideaway/TheHideAwayShopApplication.java
@@ -0,0 +1,13 @@
+package com.hideaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHideAwayShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHideAwayShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hideaway/TheHideAwayShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hideaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHideAwayShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
