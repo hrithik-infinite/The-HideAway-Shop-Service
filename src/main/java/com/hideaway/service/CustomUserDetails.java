cabea0171355c4032dbb208f87247de0ea66bd2a
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-05 21:56:28 +0530

    custom user impl to override sprinf default auth

---
+++ src/main/java/com/hideaway/service/CustomUserDetails.java
@@ -0,0 +1,35 @@
+package com.hideaway.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.hideaway.model.User;
+import com.hideaway.repository.UserRepository;
+
+@Service
+public class CustomUserDetails implements UserDetailsService {
+
+	private UserRepository userRepository;
+	public CustomUserDetails(UserRepository userRepository) {
+			this.userRepository = userRepository;
+	}
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("****No User Found****"+username);
+		}
+		
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+	}
+	
+
+}
