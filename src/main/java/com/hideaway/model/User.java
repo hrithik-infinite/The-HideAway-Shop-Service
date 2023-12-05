ef8d5387c82343a31ba78c39a131da65d1716a6e
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-05 21:29:54 +0530

    added user model

---
+++ src/main/java/com/hideaway/model/User.java
@@ -0,0 +1,56 @@
+package com.hideaway.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private String role;
+	private String mobile;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Address> addresses=new ArrayList<>();
+	
+	@Embedded
+	@ElementCollection
+	@CollectionTable(name="paymentInformation", joinColumns = @JoinColumn(name = "user_id"))
+	private List<PaymentInformation> paymentInformation = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	@JsonIgnore
+	private List<Rating> rating = new ArrayList<>();
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<Review> reviews = new ArrayList<>();
+	
+	
+	private LocalDateTime createdAt;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
