26bac9909802303668de5ab6cdb410168832ff62
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-06 21:15:30 +0530

    product service added

---
+++ src/main/java/com/hideaway/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.hideaway.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.hideaway.exception.ProductException;
+import com.hideaway.model.Product;
+import com.hideaway.request.CreateProductRequest;
+
+public interface ProductService {
+	public Product createProduct(CreateProductRequest req);
+
+	public String deleteProduct(Long productId) throws ProductException;
+
+	public Product updateProduct(Long productId, Product product) throws ProductException;
+
+	public Product findProductById(Long id) throws ProductException;
+
+	public List<Product> findProductByCategory(String category);
+
+	public Page<Product> getAllProduct(String category, List<String> colors, List<String> sizes, Integer minPrice,
+			Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber, Integer pageSize);
+}
