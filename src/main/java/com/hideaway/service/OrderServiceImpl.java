83f38a08be7edcd2fc0b71208861913b3840b816
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-07 22:13:52 +0530

    add item and order impl

---
+++ src/main/java/com/hideaway/service/OrderServiceImpl.java
@@ -0,0 +1,85 @@
+package com.hideaway.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hideaway.exception.OrderException;
+import com.hideaway.model.Address;
+import com.hideaway.model.Order;
+import com.hideaway.model.User;
+import com.hideaway.repository.CartRepository;
+import com.hideaway.repository.UserRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+	@Autowired
+	private CartRepository cartRepository;
+	
+	@Autowired
+	private CartService cartService;
+
+	@Autowired
+	private ProductService productService;
+
+	@Override
+	public Order createOrder(User user, Address shippingAdress) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order findOrderById(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> usersOrderHistory(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order placedOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order confirmedOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order shippedOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order deliveredOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order cancledOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getAllOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteOrder(Long orderId) throws OrderException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
