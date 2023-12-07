d77c1cf3714829a57cc5d69b9c3bef1e9d37088b
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-07 18:52:00 +0530

    created payment dtls

---
+++ src/main/java/com/hideaway/model/PaymentDetails.java
@@ -0,0 +1,73 @@
+package com.hideaway.model;
+
+public class PaymentDetails {
+
+	private String paymentMethod;
+	private String status;
+	private String paymentId;
+	private String razorpayPaymentLinkId;
+	private String razorpayPaymentLinkReferenceId;
+	private String razorpayPaymentLinkStatus;
+	private String razorpayPaymentId​;
+
+	public PaymentDetails() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(String paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public String getRazorpayPaymentLinkId() {
+		return razorpayPaymentLinkId;
+	}
+
+	public void setRazorpayPaymentLinkId(String razorpayPaymentLinkId) {
+		this.razorpayPaymentLinkId = razorpayPaymentLinkId;
+	}
+
+	public String getRazorpayPaymentLinkReferenceId() {
+		return razorpayPaymentLinkReferenceId;
+	}
+
+	public void setRazorpayPaymentLinkReferenceId(String razorpayPaymentLinkReferenceId) {
+		this.razorpayPaymentLinkReferenceId = razorpayPaymentLinkReferenceId;
+	}
+
+	public String getRazorpayPaymentLinkStatus() {
+		return razorpayPaymentLinkStatus;
+	}
+
+	public void setRazorpayPaymentLinkStatus(String razorpayPaymentLinkStatus) {
+		this.razorpayPaymentLinkStatus = razorpayPaymentLinkStatus;
+	}
+
+	public String getRazorpayPaymentId​() {
+		return razorpayPaymentId​;
+	}
+
+	public void setRazorpayPaymentId​(String razorpayPaymentId​) {
+		this.razorpayPaymentId​ = razorpayPaymentId​;
+	}
+
+}
