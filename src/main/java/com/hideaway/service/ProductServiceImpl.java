a2e55e877aa7e97ffcfbbb7680b10a77f2411c4c
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-07 18:36:40 +0530

    completed product services

--- src/main/java/com/hideaway/service/ProductServiceImpl.java
+++ src/main/java/com/hideaway/service/ProductServiceImpl.java
@@ -2,9 +2,14 @@ package com.hideaway.service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.hideaway.exception.ProductException;
@@ -20,8 +25,8 @@ public class ProductServiceImpl implements ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    @Autowired
-    private UserService userService;
+     @Autowired
+     private UserService userService;
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -57,49 +62,61 @@ public class ProductServiceImpl implements ProductService {
 
             thirdLevel = categoryRepository.save(thirdLavelCategory);
         }
-        
+
         Product product = new Product();
-		product.setTitle(req.getTitle());
-		product.setColor(req.getColor());
-		product.setDescription(req.getDescription());
-		product.setDiscountedPrice(req.getDiscountedPrice());
-		product.setDiscountPersent(req.getDiscountPercent());
-		product.setImageUrl(req.getImageUrl());
-		product.setBrand(req.getBrand());
-		product.setPrice(req.getPrice());
-		product.setSizes(req.getSize());
-		product.setQuantity(req.getQuantity());
-		product.setCategory(thirdLevel);
-		product.setCreatedAt(LocalDateTime.now());
+        product.setTitle(req.getTitle());
+        product.setColor(req.getColor());
+        product.setDescription(req.getDescription());
+        product.setDiscountedPrice(req.getDiscountedPrice());
+        product.setDiscountPersent(req.getDiscountPercent());
+        product.setImageUrl(req.getImageUrl());
+        product.setBrand(req.getBrand());
+        product.setPrice(req.getPrice());
+        product.setSizes(req.getSize());
+        product.setQuantity(req.getQuantity());
+        product.setCategory(thirdLevel);
+        product.setCreatedAt(LocalDateTime.now());
 
-		Product savedProduct = productRepository.save(product);
+        Product savedProduct = productRepository.save(product);
 
-		System.out.println("products - " + product);
+        System.out.println("products - " + product);
 
-		return savedProduct;
+        return savedProduct;
     }
 
     @Override
     public String deleteProduct(Long productId) throws ProductException {
-    	Product product = findProductById(productId);
+        Product product = findProductById(productId);
 
-		System.out.println("delete product " + product.getId() + " - " + productId);
-		product.getSizes().clear();
-		productRepository.delete(product);
+        System.out.println("delete product " + product.getId() + " - " + productId);
+        product.getSizes().clear();
+        productRepository.delete(product);
 
-		return "Product deleted Successfully";
+        return "Product deleted Successfully";
     }
 
     @Override
-    public Product updateProduct(Long productId, Product product) throws ProductException {
-        // TODO Auto-generated method stub
-        return null;
+    public Product updateProduct(Long productId, Product req) throws ProductException {
+        Product product = findProductById(productId);
+
+        if (req.getQuantity() != 0) {
+            product.setQuantity(req.getQuantity());
+        }
+        if (req.getDescription() != null) {
+            product.setDescription(req.getDescription());
+        }
+
+        return productRepository.save(product);
     }
 
     @Override
     public Product findProductById(Long id) throws ProductException {
-        // TODO Auto-generated method stub
-        return null;
+        Optional<Product> opt = productRepository.findById(id);
+
+        if (opt.isPresent()) {
+            return opt.get();
+        }
+        throw new ProductException("product not found with id " + id);
     }
 
     @Override
@@ -111,8 +128,32 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Page<Product> getAllProduct(String category, List<String> colors, List<String> sizes, Integer minPrice,
             Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber, Integer pageSize) {
-        // TODO Auto-generated method stub
-        return null;
+
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        List<Product> products = productRepository.filterProducts(category, minPrice, maxPrice, minDiscount, sort);
+
+        if (!colors.isEmpty()) {
+            products = products.stream()
+                    .filter(p -> colors.stream().anyMatch(c -> c.equalsIgnoreCase(p.getColor())))
+                    .collect(Collectors.toList());
+
+        }
+
+        if (stock != null) {
+
+            if (stock.equals("in_stock")) {
+                products = products.stream().filter(p -> p.getQuantity() > 0).collect(Collectors.toList());
+            } else if (stock.equals("out_of_stock")) {
+                products = products.stream().filter(p -> p.getQuantity() < 1).collect(Collectors.toList());
+            }
+
+        }
+        int startIndex = (int) pageable.getOffset();
+        int endIndex = Math.min(startIndex + pageable.getPageSize(), products.size());
+
+        List<Product> pageContent = products.subList(startIndex, endIndex);
+        Page<Product> filteredProducts = new PageImpl<>(pageContent, pageable, products.size());
+        return filteredProducts; // If color list is empty, do nothing and return all products
     }
 
 }
