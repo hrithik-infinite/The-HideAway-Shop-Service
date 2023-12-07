94dabcd863fbc911a209c45ff7a0f0e90013b6a9
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-07 23:08:21 +0530

    added product service controller

--- src/main/java/com/hideaway/service/ProductService.java
+++ src/main/java/com/hideaway/service/ProductService.java
@@ -21,4 +21,6 @@ public interface ProductService {
 
 	public Page<Product> getAllProduct(String category, List<String> colors, List<String> sizes, Integer minPrice,
 			Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber, Integer pageSize);
+
+	public List<Product> searchProduct(String q);
 }
