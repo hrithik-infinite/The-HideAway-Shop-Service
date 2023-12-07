b542a1c7bd544aad48b20f2d4236daa416b7c40a
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-07 18:52:16 +0530

    order services added '

---
+++ src/main/java/com/hideaway/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.hideaway.service;
+
+import java.util.List;
+
+import com.hideaway.exception.OrderException;
+import com.hideaway.model.Address;
+import com.hideaway.model.Order;
+import com.hideaway.model.User;
+
+public interface OrderService {
+
+	public Order createOrder(User user, Address shippingAdress);
+
+	public Order findOrderById(Long orderId) throws OrderException;
+
+	public List<Order> usersOrderHistory(Long userId);
+
+	public Order placedOrder(Long orderId) throws OrderException;
+
+	public Order confirmedOrder(Long orderId) throws OrderException;
+
+	public Order shippedOrder(Long orderId) throws OrderException;
+
+	public Order deliveredOrder(Long orderId) throws OrderException;
+
+	public Order cancledOrder(Long orderId) throws OrderException;
+
+	public List<Order> getAllOrders();
+
+	public void deleteOrder(Long orderId) throws OrderException;
+}
