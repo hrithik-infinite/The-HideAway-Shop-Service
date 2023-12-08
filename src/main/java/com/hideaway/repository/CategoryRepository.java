b1f2b002d1e4777e7ea403f2577e7b06aabe9009
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-08 22:58:52 +0530

    fixed typo

--- src/main/java/com/hideaway/repository/CategoryRepository.java
+++ src/main/java/com/hideaway/repository/CategoryRepository.java
@@ -11,6 +11,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 	public Category findByName(String name);
 
 	@Query("Select c from Category c where c.name=:name AND c.parentCategory.name=:parentCategoryName")
-	public Category findByNameAndParant(@Param("name") String name,
+	public Category findByNameAndParent(@Param("name") String name,
 			@Param("parentCategoryName") String parentCategoryName);
 }
