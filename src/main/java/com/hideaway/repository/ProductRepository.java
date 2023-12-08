4e7feaf6d42c16ddd440c401d2a16428b427b894
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-08 22:58:18 +0530

    added methods in product repo

--- src/main/java/com/hideaway/repository/ProductRepository.java
+++ src/main/java/com/hideaway/repository/ProductRepository.java
@@ -13,7 +13,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	@Query("SELECT p FROM Product p " +
 			"WHERE (p.category.name = :category OR :category = '') " +
 			"AND ((:minPrice IS NULL AND :maxPrice IS NULL) OR (p.discountedPrice BETWEEN :minPrice AND :maxPrice)) " +
-			"AND (:minDiscount IS NULL OR p.discountPersent >= :minDiscount) " +
+			"AND (:minDiscount IS NULL OR p.discountPercent >= :minDiscount) " +
 			"ORDER BY " +
 			"CASE WHEN :sort = 'price_low' THEN p.discountedPrice END ASC, " +
 			"CASE WHEN :sort = 'price_high' THEN p.discountedPrice END DESC")
@@ -23,4 +23,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 			@Param("maxPrice") Integer maxPrice,
 			@Param("minDiscount") Integer minDiscount,
 			@Param("sort") String sort);
+
+	@Query("SELECT p From Product p Where LOWER(p.category.name)=:category")
+	public List<Product> findByCategory(@Param("category") String category);
+	
+	@Query("SELECT p From Product p where LOWER(p.title) Like %:query% OR LOWER(p.description) Like %:query% OR LOWER(p.brand) LIKE %:query% OR LOWER(p.category.name) LIKE %:query%")
+	public List<Product> searchProduct(@Param("query")String query);
 }
