3ceac160986b2ce4af8dbfd5ecc75f44fe985256
Hrithik Agarwal <hrithikinfinite@gmail.com>
2023-12-09 12:48:38 +0530

    fix for create order api

--- src/main/java/com/hideaway/model/Order.java
+++ src/main/java/com/hideaway/model/Order.java
@@ -18,8 +18,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="orders")
 public class Order {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
